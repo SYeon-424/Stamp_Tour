3c74cdea08b98c6d761f46a9598acfb3ec5daa32
S.Yeon <angielsy424024@gmail.com>
2025-07-21 07:57:50 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pyrebase
+firebase_config = {
+    "apiKey": "AIzaSyCswFmrOGU3FyLYxwbNPTp7hvQxLfTPIZw",
+        "authDomain": "sw-projects-49798.firebaseapp.com",
+            "databaseURL": "https://sw-projects-49798-default-rtdb.firebaseio.com",
+                "projectId": "sw-projects-49798",
+                    "storageBucket": "sw-projects-49798.firebasestorage.app",
+                        "messagingSenderId": "812186368395",
+                            "appId": "1:812186368395:web:be2f7291ce54396209d78e"
+                            }
+
+                            firebase = pyrebase.initialize_app(firebase_config)
+                            auth = firebase.auth()
+                            firestore = firebase.database()
+                            storage = firebase.storage()
+
+                            # ---------------------
+                            # 세션 상태 초기화
+                            # ---------------------
+                            if "logged_in" not in st.session_state:
+                                st.session_state.logged_in = False
+                                    st.session_state.user_email = ""
+                                        st.session_state.id_token = ""
+                                            st.session_state.user_name = ""
+                                                st.session_state.user_gender = "선택 안함"
+                                                    st.session_state.user_phone = ""
+                                                        st.session_state.profile_image_url = ""
